3c2792a67f72ba67e140e1e203abf4d8927c6c5e
olamike2021 <agboolafolorunso4@gmail.com>
2023-04-08 09:53:11 +0200

    rebuild this new code for python

--- python.py
+++ python.py
@@ -2,3 +2,21 @@ name = "Michael"
 age = 20
 pi = 3.14
 Numbers = [1, 2, 3, 4]
+
+a = 21
+
+b = 10
+
+print(a + b)
+
+print(a - b)
+
+print(a * b)
+
+print(a / b)
+
+print(a % b)
+
+print(a ** b)
+
+print(a // b)
